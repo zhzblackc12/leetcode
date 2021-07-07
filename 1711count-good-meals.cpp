2e3245373e2c6f343a04648ea9d0ae35e3d2aea3
zhz <gh_zhz@ghgame.cn>
2021-07-07 23:03:26 +0800

    1711count-good-meals.cpp

---
+++ 1711count-good-meals.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int countPairs(vector<int>& deliciousness) {
+        int maxValue = 0;
+        for (int i = 0; i < deliciousness.size(); i++)
+        {
+            int item = deliciousness[i];
+            if (item > maxValue)
+            {
+                maxValue = item;
+            }
+        }
+
+        int maxSum = 2 * maxValue;        
+        int retValue = 0;
+        unordered_map<int, int> deliciousMap;
+        for (int i = 0; i < deliciousness.size(); i++)
+        {
+            int item = deliciousness[i];
+            for (int sum = 1; sum <= maxSum; sum <<= 1)
+            {
+                int foundValue = sum - item;
+                int count = deliciousMap.count(foundValue) == 0 ? 0 : deliciousMap[foundValue];
+                retValue = (retValue + count) %  MOD;                
+            }
+
+            deliciousMap[item]++;
+        }
+
+        return retValue;
+    }
+
+    const int MOD = 1000000007;
+
+};
+
+
+void test()
+{
+    Solution solution;
+    vector<int> deliciousness = {1,3,5,7,9};
+    int nums = solution.countPairs(deliciousness);
+    cout << nums << " " << 4 << endl;
+
+    deliciousness = {1,1,1,3,3,3,7};
+    nums = solution.countPairs(deliciousness);
+    cout << nums << " " << 15 << endl;
+}
+
+int main(int argc, char* argv[])
+{
+    test();
+    return 0;
+}
